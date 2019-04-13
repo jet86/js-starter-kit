e94eb51cb6b2fad3caad6d720b82c8930cef19a8
Tim Jones <tim@timjones.id.au>
2019-04-13 13:46:30 +1000

    Initial changes to make interface a faucet

--- src/plasma-faucet.js
+++ src/plasma-faucet.js
@@ -1,20 +1,23 @@
+const TOKEN_CONTRACT_ADDRESS = '0x65d82f6ff66dbc4e455b36055f682619a05abb9b'
+const PLASMA_FAUCET_ADDRESS = '0xcf9567cc6041d262387a1b0a03dd7a64fcbf9fd8'
+const PLASMA_FAUCET_VALUE = '1000000000000000000'
+
 async function childchainTransfer () {
-  var fromAddr = document.getElementById('transferFromAddress').value
+  var fromAddr = PLASMA_FAUCET_ADDRESS
   var toAddr = document.getElementById('transferToAddress').value
-  var tokenContract = document.getElementById('transferContractAddress').value
-  tokenContract = tokenContract || OmgUtil.transaction.ETH_CURRENCY
-  var value = document.getElementById('transferValue').value
+  var tokenContract = TOKEN_CONTRACT_ADDRESS
+  var value = PLASMA_FAUCET_VALUE
 
   const utxos = await childChain.getUtxos(fromAddr)
   const utxosToSpend = selectUtxos(utxos, value, tokenContract)
   if (!utxosToSpend) {
-    alert(`No utxo big enough to cover the amount ${value}`)
+    alert(`The faucet is currently out of tokens. Please contact the faucet operator.`)
     return
   }
   
   const utxosForFee = selectUtxos(utxos, 1, OmgUtil.transaction.ETH_CURRENCY)
   if (!utxosForFee) {
-    alert(`No utxo with ETH to act as dummy fee. Please add at least 1 wei ETH to the from account.`)
+    alert(`No utxo with ETH to act as dummy fee. Please add at least 1 wei ETH to the faucet address.`)
     return
   }
   
@@ -67,4 +70,39 @@ async function childchainTransfer () {
     const result = await childChain.submitTransaction(signedTx)
     console.log(`Submitted transaction: ${JSON.stringify(result)}`)
   })
+}
+
+async function showFaucetBalance () {
+  web3.eth.getBalance(PLASMA_FAUCET_ADDRESS, (err, ethBalance) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    document.getElementById('faucetRootchainBalance').innerHTML += '<div>' + PLASMA_FAUCET_ADDRESS + ': ' + (ethBalance / 1.0e18) + ' ETH </div>'
+  })
+  childChain.getBalance(PLASMA_FAUCET_ADDRESS).then(childchainBalance => {
+    document.getElementById('faucetChildchainBalance').innerHTML += '<div>' + PLASMA_FAUCET_ADDRESS + ': ' + JSON.stringify(childchainBalance) + '</div>'
+  })
+}
+
+async function showBalances () {
+  var addresses = globalKeystore.getAddresses()
+  document.getElementById('rootchainBalance').innerHTML = 'Retrieving addresses...'
+
+  if (addresses.length > 0) {
+    document.getElementById('rootchainBalance').innerHTML = ''
+    document.getElementById('childchainBalance').innerHTML = ''
+    addresses.forEach(async (address) => {
+      web3.eth.getBalance(address, (err, ethBalance) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        document.getElementById('rootchainBalance').innerHTML += '<div>' + address + ': ' + (ethBalance / 1.0e18) + ' ETH </div>'
+      })
+      childChain.getBalance(address).then(childchainBalance => {
+        document.getElementById('childchainBalance').innerHTML += '<div>' + address + ': ' + JSON.stringify(childchainBalance) + '</div>'
+      })
+    })
+  }
 }
