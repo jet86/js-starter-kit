427623eb7af8ae36c8b2dc2ff9b4ca3880ff6480
Tim Jones <tim@timjones.id.au>
2019-05-23 13:01:15 +1000

    Initial template for Merge and Exit

---
+++ src/mergeandexit.js
@@ -0,0 +1,91 @@
+const DUMMY_PASSWORD = '123'
+
+let SHOW_ADDRESSES = 2
+
+function loadWalletAddress () {
+  var inputWalletAddress = document.getElementById('seed').value
+  
+  //console.log("Seed phrase entered: " + inputWalletAddress)
+  if(document.getElementById('showAddresses').value != '') {
+    SHOW_ADDRESSES = Number(document.getElementById('showAddresses').value)
+  }
+  loadUtxoVault(DUMMY_PASSWORD, document.getElementById('seed').value, SHOW_ADDRESSES)
+  document.getElementById('seed').value = ''
+  //document.getElementById('showAddresses').value = ''
+
+  document.getElementById('addressArea').style.display = 'block'
+  document.getElementById('utxoArea').style.display = 'none'
+  document.getElementById('LogArea').style.display = 'none'
+
+}
+
+async function showAddresses () {
+  var addresses = globalKeystore.getAddresses()
+
+  document.getElementById('childchainBalance').innerHTML = ''
+  for(let addressIndex in addresses) {
+    //console.log('Index: ' + addressIndex + ' Address: ' + addresses[addressIndex])
+
+    document.getElementById('childchainBalance').innerHTML += '<div>' + addressIndex + ': ' + addresses[addressIndex] + ': ' + await getBalanceOfToken(addresses[addressIndex], OmgUtil.transaction.ETH_CURRENCY) + ' wei ETH and ' + await getCountOfToken(addresses[addressIndex], false) + ' ERC20 tokens, across <a href="#" onclick="return showUtxos(\'' + addresses[addressIndex] + '\')">' + await getUtxoCount(addresses[addressIndex]) + ' UTXOs</a></div>'
+  }
+}
+
+function loadUtxoVault (vPass, vSeed, vDepth) {
+  lightwallet.keystore.createVault({
+    password: vPass,
+    seedPhrase: vSeed,
+    hdPathString: "m/0'/0'/0'"
+  }, function (err, keystore) {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    globalKeystore = keystore
+
+    const web3Provider = new HookedWeb3Provider({
+      host: WEB3_PROVIDER_URL,
+      transaction_signer: globalKeystore
+    })
+    web3.setProvider(web3Provider)
+
+    rootChain = new RootChain(web3, PLASMA_CONTRACT_ADDRESS)
+    childChain = new ChildChain(WATCHER_URL, CHILDCHAIN_URL)
+
+    globalKeystore.keyFromPassword(vPass, function (err, pwDerivedKey) {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      // Generate the addresses in the wallet
+      globalKeystore.generateNewAddress(pwDerivedKey, vDepth)
+      showAddresses()
+    })
+  })
+}
+
+async function showUtxos (utxoAddress) {
+  document.getElementById('utxoAddress').innerHTML = ''
+  document.getElementById('utxoDetails').innerHTML = '' 
+
+  //console.log(utxoAddress)
+  document.getElementById('mergeUTXObutton').style.display = 'block'
+  document.getElementById('utxoArea').style.display = 'block'
+  document.getElementById('LogArea').style.display = 'none'
+  document.getElementById('utxoAddress').innerHTML = utxoAddress + ' contains ' + await getUtxoCount(utxoAddress) + ' UTXOs'
+
+  const utxos = await childChain.getUtxos(utxoAddress)
+
+  //console.log(JSON.stringify(utxos))
+
+  document.getElementById('utxoDetails').innerHTML = '<pre>' + JSON.stringify(utxos, null, 2) + '</pre>'
+}
+
+async function mergeAddressUTXOs () {
+  document.getElementById('mergeUTXObutton').style.display = 'none'
+  document.getElementById('utxoDetails').innerHTML = 'Merging...'
+  document.getElementById('LogArea').style.display = 'block'
+
+  //
+}
